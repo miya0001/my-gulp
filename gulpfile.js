0ccf0193a52dee56b5ec964afb626ebb099db66e
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-12-20 23:30:25 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var csso = require('gulp-csso');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
 
-gulp.task('js', function(){
+gulp.task('js', gulp.series(() => {
 	return gulp.src(["src/*.js"])
 		.pipe(concat('script.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('./js'));
-});
+}));
 
-gulp.task('sass', function () {
+gulp.task('sass', gulp.series(() => {
 	return gulp.src('./src/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./src'));
-});
+}));
 
-gulp.task('css', ['sass'], function(){
+gulp.task('css', gulp.series(gulp.parallel('sass'), () => {
 	return gulp.src(["src/*.css"])
 		.pipe(concat('style.min.css'))
 		.pipe(minifyCSS())
 		.pipe(csso())
 		.pipe(gulp.dest('./css'));
-});
+}));
 
-gulp.task('default', ['js', 'css']);
+gulp.task('default', gulp.series(gulp.parallel('js', 'css')));
